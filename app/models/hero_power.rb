6b5d9a413a7527f61a75fa35cef8625124514eb1
david <davmwa90@gmail.com>
2023-03-25 16:32:38 +0300

    added the validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -2,4 +2,6 @@ class HeroPower < ApplicationRecord
     #defining relationships 
     belongs_to :hero 
     belongs_to :power 
+
+    validates :strength, numericality: { only_integer: true}, inclusion: { in: %w(strong weak average)}
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -2,4 +2,6 @@ class Power < ApplicationRecord
        #defining relationships 
        has_many :hero_powers 
        has_many :heroes, through: :hero_powers 
+
+       validates :power, length: [minimum: 20]
 end
